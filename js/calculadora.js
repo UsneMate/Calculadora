18d95af7ee8d69a0719d9c997564a31c03cf50b8
UsneMate <mmanzano@inscaparrella.cat>
2024-11-06 20:28:12 +0100

    info

---
+++ js/calculadora.js
@@ -0,0 +1,11 @@
+
+//funcions per recordar la pàgina anterior i la pàgina següent
+function goBack() {
+    window.history.back();
+}
+
+function goForward() {
+    window.history.forward();
+}
+
+// Obtenir i mostrar la informació del navegador
